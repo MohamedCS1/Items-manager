b39360fd796c1b9dab792222f4e4188d830676e5
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2022-10-04 21:03:28 +0100

    create bottom nav view

--- app/src/main/java/com/example/p_scanner/MainActivity.kt
+++ app/src/main/java/com/example/p_scanner/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.p_scanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.p_scanner.databinding.ActivityMainBinding
+import com.example.p_scanner.databinding.ActivitySplashScreenBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.itemIconTintList = null
+
+        binding.bottomNavigationView.setOnItemSelectedListener(object :NavigationBarView.OnItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                return true
+            }
+        })
+        binding.bottomNavigationView.setOnItemReselectedListener(object :NavigationBarView.OnItemReselectedListener{
+            override fun onNavigationItemReselected(item: MenuItem) {
+            }
+        })
     }
 }
