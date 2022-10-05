3d0e975828c3344f5f108dda0e86c5aafc3205db
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2022-10-05 18:21:43 +0100

    create fragments and init

--- app/src/main/java/com/example/p_scanner/MainActivity.kt
+++ app/src/main/java/com/example/p_scanner/MainActivity.kt
@@ -4,21 +4,33 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
 import com.example.p_scanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding:ActivityMainBinding
+
+    lateinit var scanningFragment: ScanningFragment
+    lateinit var listProductsFragment: ListProductsFragment
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        scanningFragment = ScanningFragment()
+        listProductsFragment = ListProductsFragment()
+
+
         binding.buScanner.setOnClickListener {
             binding.buScanner.setBackgroundResource(R.drawable.background_bu_scanner)
             binding.buListProducts.setBackgroundColor(android.R.color.transparent)
             binding.buListProducts.setColorFilter(Color.parseColor("#BBBBBB"))
             binding.buScanner.setColorFilter(Color.parseColor("#5A6CF3"))
+            setFragment(scanningFragment)
         }
 
         binding.buListProducts.setOnClickListener {
@@ -26,6 +38,14 @@ class MainActivity : AppCompatActivity() {
             binding.buScanner.setBackgroundColor(android.R.color.transparent)
             binding.buScanner.setColorFilter(Color.parseColor("#BBBBBB"))
             binding.buListProducts.setColorFilter(Color.parseColor("#F08F5F"))
+            setFragment(listProductsFragment)
         }
+    }
+
+    private fun setFragment(fragment: Fragment)
+    {
+        val fr = supportFragmentManager.beginTransaction()
+        fr.replace(R.id.fragment_container ,fragment)
+        fr.commit()
     }
 }
